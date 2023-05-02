89d0820367e5fee430287bd18a797904e0cf29b7
Nur Hikmah <44131310+nrhikmah@users.noreply.github.com>
2023-05-02 09:52:45 +0800

    Update symbols.py

--- text/symbols.py
+++ text/symbols.py
@@ -7,9 +7,9 @@ The default is a set of ASCII characters that works well for English or text tha
 from text import cmudict
 
 _pad        = '_'
-_punctuation = '!\'(),.:;? '
+_punctuation = '!\'(),.:;?᨟᨞ '
 _special = '-'
-_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+_letters = 'ᨀᨀᨗᨀᨘᨀᨙᨀᨚᨀᨛᨁᨁᨗᨁᨘᨁᨙᨁᨚᨁᨛᨂᨂᨗᨂᨘᨂᨙᨂᨚᨂᨛᨃᨃᨗᨃᨘᨃᨙᨃᨚᨃᨛᨄᨄᨗᨄᨘᨄᨙᨄᨚᨄᨛᨅᨅᨗᨅᨘᨅᨙᨅᨚᨅᨛᨆᨆᨗᨆᨘᨆᨙᨆᨚᨆᨛᨇᨇᨗᨇᨘᨇᨙᨇᨛᨈᨈᨗᨈᨘᨈᨙᨈᨚᨈᨛᨉᨉᨗᨉᨘᨉᨙᨉᨚᨉᨛᨊᨊᨗᨊᨘᨊᨙᨊᨚᨊᨛᨋᨋᨗᨋᨘᨋᨙᨋᨚᨋᨛᨌᨌᨗᨌᨘᨌᨙᨌᨚᨌᨛᨍᨍᨗᨍᨘᨍᨙᨍᨚᨍᨛᨎᨎᨗᨎᨘᨎᨙᨎᨚᨎᨛᨏᨏᨗᨏᨘᨏᨙᨏᨚᨏᨛᨐᨐᨗᨐᨘᨐᨙᨐᨚᨐᨛᨑᨑᨗᨑᨘᨑᨙᨑᨚᨑᨛᨒᨒᨗᨒᨘᨒᨙᨒᨚᨒᨛᨓᨓᨗᨓᨘᨓᨙᨓᨚᨓᨛᨔᨔᨗᨔᨘᨔᨙᨔᨚᨔᨛᨕᨕᨗᨕᨘᨕᨙᨕᨚᨕᨛᨖᨖᨗᨖᨘᨖᨙᨖᨚᨖ'
 
 # Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
 _arpabet = ['@' + s for s in cmudict.valid_symbols]
